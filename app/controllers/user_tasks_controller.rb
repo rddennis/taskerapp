405613cd2364b39e26203ede565be830d25fa4b1
rddennis <ronesha_dennis@yahoo.com>
2016-05-24 08:56:58 -0500

    Updates user accessibility to user tasks in user tasks controller

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -72,6 +72,16 @@ class UserTasksController < ApplicationController
 
     def set_user_tasks
       @user_tasks = UserTask.order(:due_date)
+      if current_user.id == @user_task.user_id
+        @user_task
+      else
+        invalid_task
+      end
+    end
+
+    def invalid_task
+      logger.error "Attempt to access invalid task #{params[:id]}"
+      redirect_to user_tasks_url, notice: "Invalid task. Access denied."
     end
     # Use callbacks to share common setup or constraints between actions.
     def set_user_task
@@ -81,5 +91,9 @@ class UserTasksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_task_params
       params.require(:user_task).permit(:description, :due_date)
+    end
+
+    def set_all_tasks
+      @user_tasks = UserTask.where(user_id: current_user.id).order(:due)
     end
 end
