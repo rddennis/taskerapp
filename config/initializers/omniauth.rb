7cfd15baaac75a0da87168a1d1cfc6f77d1cf398
rddennis <ronesha_dennis@yahoo.com>
2016-06-05 15:52:55 -0500

    removes line from OmniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
-
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
